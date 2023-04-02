1befb554862197ac9fc58557b588c738d79a17b7
snOlga <olga.safonova@outlook.com>
2023-04-02 16:46:46 +0300

    pseudo client-server

---
+++ src/ClientServer/GettingPort.java
@@ -0,0 +1,24 @@
+package ClientServer;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public interface GettingPort
+{
+    static int getPort()
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Write a port (in integer format, more than 1024):");
+        try
+        {
+            int number = reader.nextInt();
+            return number;
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println("Not right input. Try again.");
+            getPort();
+            return 0;
+        }
+    }
+}
