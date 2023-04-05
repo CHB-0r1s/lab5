6d877f5d40dba882add929eeceadd5a87088a12b
snOlga <olga.safonova@outlook.com>
2023-04-05 17:45:49 +0300

    execute_script arraylist field in command from client

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -6,8 +6,10 @@ import Utils.MyReaders.MyLongReader;
 import Utils.MyReaders.MyPrimDoubleReader;
 import Utils.SpaceMarineCreator;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public abstract class Command implements Serializable
 {
@@ -17,6 +19,7 @@ public abstract class Command implements Serializable
     private long longFromClient;
     private double doubleFromClient;
     private SpaceMarine spaceMarineFromClient;
+    private ArrayList<Command> commandsFromScript;
 
     public void setFloatFromClient()
     {
@@ -26,11 +29,33 @@ public abstract class Command implements Serializable
     {
         this.longFromClient = MyLongReader.read("Enter ID in long format: ");
     }
+
+    public void setLongFromClient(long longFromClient)
+    {
+        this.longFromClient = longFromClient;
+    }
+
     public void setDoubleFromClient() {this.doubleFromClient = MyPrimDoubleReader.read("Enter HP in double format: ");
     }
+
+    public void setDoubleFromClient(double doubleFromClient)
+    {
+        this.doubleFromClient = doubleFromClient;
+    }
+
     public void setSpaceMarineFromClient()
     {
         this.spaceMarineFromClient = SpaceMarineCreator.createSpaceMarine();
+    }
+
+    public void setSpaceMarineFromClient(SpaceMarine spaceMarineFromClient)
+    {
+        this.spaceMarineFromClient = spaceMarineFromClient;
+    }
+
+    public void setCommandsFromScript(String fileName) throws FileNotFoundException
+    {
+        this.commandsFromScript = Invoker.invokeCommandFromScriptForClient(fileName);
     }
 
     public float getFloatFromClient()
@@ -47,5 +72,10 @@ public abstract class Command implements Serializable
     public SpaceMarine getSpaceMarineFromClient()
     {
         return spaceMarineFromClient;
+    }
+
+    public ArrayList<Command> getCommandsFromScript()
+    {
+        return commandsFromScript;
     }
 }
--- src/Command/Invoker.java
+++ src/Command/Invoker.java
@@ -1,16 +1,22 @@
 package Command;
 
+import BaseObjects.SpaceMarine;
 import Command.ConcreteCommands.*;
+import Utils.ManagerOfCollection;
+import Utils.SpaceMarineCreator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class Invoker implements Serializable
 {
-    public HashMap<String, Command> invokerHashMap = new HashMap<String, Command>();
+    public static HashMap<String, Command> invokerHashMap = new HashMap<String, Command>();
     //why is it cannot be static^^^^
     public ArrayList<String> invokerListOfCommand = new ArrayList<>();
 
@@ -41,6 +47,21 @@ public class Invoker implements Serializable
                 case "Update": command.setLongFromClient(); command.setSpaceMarineFromClient(); break;
                 case "RemoveAllByHealth": command.setDoubleFromClient(); break;
                 case "RemoveByID": command.setLongFromClient(); break;
+                case "ExecuteScript": if (command_name.length > 1)
+                {
+                    try
+                    {
+                        command.setCommandsFromScript(command_name[1]);
+                    } catch (FileNotFoundException e)
+                    {
+                        System.out.println("There is no file with such name. Try again.");
+                        return null;
+                    }
+                }else
+                {
+                    System.out.println("You have not entered a file name.");
+                    return null;
+                } break;
             }
             invokerListOfCommand.add(command_name[0]);
             return command;
@@ -54,6 +75,91 @@ public class Invoker implements Serializable
             return null;
         }
     }
+
+    public static ArrayList<Command> invokeCommandFromScriptForClient(String fileName) throws FileNotFoundException
+    {
+        File file = new File(fileName);
+        Scanner reader = new Scanner(file);
+        String line;
+        String commandLine;
+        ArrayList<String> parameters = new ArrayList<>();
+        ArrayList<Command> commands = new ArrayList<>();
+        Invoker invoker = new Invoker();
+        outer:
+        while (reader.hasNextLine())
+        {
+            line = reader.nextLine();
+            if (line.split(" ")[0].matches("add|update|remove_greater|remove_lower"))
+            {
+                commandLine = line;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (reader.hasNextLine())
+                    {
+                        line = reader.nextLine();
+                        parameters.add(line);
+                    }
+                    else
+                    {
+                        System.out.println("There are not enough parameters to create an object.");
+                        break outer;
+                    }
+                }
+                SpaceMarine spaceMarine = SpaceMarineCreator.createScriptSpaceMarine(parameters);
+                Command command = invokerHashMap.get(commandLine.split(" ")[0]);
+                switch (command.getClass().getSimpleName().toString())
+                {
+                    case "Add":
+                    case "RemoveGreater":
+                    case "RemoveLower":
+                        command.setSpaceMarineFromClient(spaceMarine); break;
+                    case "Update": try
+                    {
+                        command.setLongFromClient(Long.parseLong(commandLine.split(" ")[1]));
+                        command.setSpaceMarineFromClient(spaceMarine); break;
+                    }catch (NumberFormatException | ArrayIndexOutOfBoundsException e)
+                    {
+                        System.out.println("There is no number after Update.");
+                        break outer;
+                    }
+
+                    case "RemoveAllByHealth": try
+                    {
+                        command.setDoubleFromClient(Double.parseDouble(commandLine.split(" ")[1]));
+                    }catch (NumberFormatException | ArrayIndexOutOfBoundsException e)
+                    {
+                        System.out.println("There is no number after Remove_All_By_Health.");
+                        break outer;
+                    }
+
+                    case "RemoveByID": try
+                    {
+                        command.setLongFromClient(Long.parseLong(commandLine.split(" ")[1]));
+                        command.setSpaceMarineFromClient(spaceMarine); break;
+                    }catch (NumberFormatException | ArrayIndexOutOfBoundsException e)
+                    {
+                        System.out.println("There is no number after Update.");
+                        break outer;
+                    }
+
+                    case "ExecuteScript": command = null; break;
+                }
+                commands.add(command);
+            }
+//            else if (line.split(" ")[0].equals("execute_script")
+//                    && line.split(" ")[1].equals(ExecuteScript.getPath())) { System.out.println("An attempt to recursively call the script was stopped."); }
+            else { try
+            {
+                commands.add(invoker.invokeForClient(line.split(" ")));
+            } catch (IOException e)
+            {
+                System.out.println(e);
+            }
+            }
+        }
+        return commands;
+    }
+
 
     private void fillHashMap()
     {
