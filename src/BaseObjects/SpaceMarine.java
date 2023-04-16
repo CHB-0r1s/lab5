e2c93cf1b765cfab61ce9327ceef065678a5828a
snOlga <olga.safonova@outlook.com>
2023-04-16 18:34:08 +0300

    null id in obj from client

--- src/BaseObjects/SpaceMarine.java
+++ src/BaseObjects/SpaceMarine.java
@@ -64,7 +64,7 @@ public class SpaceMarine implements Comparable<SpaceMarine>, Serializable
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
